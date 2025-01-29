ad277f37b9b27e320dcb850e311a5f679bb46fa3
Piyush Chhoriya <piyushchhoriya98@gmial.com>
2025-01-28 22:00:33 -0500

    BFS code commit

--- Binary-Tree-Lavel-Order-Traversal.java
+++ Binary-Tree-Lavel-Order-Traversal.java
@@ -44,3 +44,31 @@ class Solution {
         return result;
     }
 }
+
+// Traversal Using DFS
+// Time Complexity : O(n)
+//Space Complexity : O(h)
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null){
+            return new ArrayList<>();
+        }
+        result=new ArrayList<>();
+        dfs(root,0);
+        return result;
+    }
+    private void dfs(TreeNode root, int level){
+        if(root == null){
+            return;
+        }
+        if(result.size()==level){
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+
+        dfs(root.left,level+1);
+        dfs(root.right,level+1);
+
+    }
+}
